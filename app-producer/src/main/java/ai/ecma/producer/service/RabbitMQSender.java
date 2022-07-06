6520d6428fffabeef33575e58b45f40a3072a40a
Muhammad <shaymanov.2000@gmail.com>
2022-07-06 13:55:28 +0500

    Bug fixes

--- app-producer/src/main/java/ai/ecma/producer/service/RabbitMQSender.java
+++ app-producer/src/main/java/ai/ecma/producer/service/RabbitMQSender.java
@@ -17,54 +17,11 @@ import org.springframework.stereotype.Service;
 public class RabbitMQSender {
     private final RabbitTemplate rabbitTemplate;
 
-
-    @Value("${spring.rabbitmq.queues.default.name}")
-    private String defaultQueueName;
-
-    @Value("${spring.rabbitmq.queues.default.durable}")
-    private boolean defaultQueueDurable;
-
     @Value("${spring.rabbitmq.queues.default.routing-key}")
     private String defaultQueueRoutingKey;
 
     @Value("${spring.rabbitmq.queues.default.exchange}")
     private String defaultQueueExchange;
-
-    @Value("${spring.rabbitmq.queues.notification.name}")
-    private String notificationQueueName;
-
-    @Value("${spring.rabbitmq.queues.notification.durable}")
-    private boolean notificationQueueDurable;
-
-    @Value("${spring.rabbitmq.queues.notification.routing-key}")
-    private String notificationQueueRoutingKey;
-
-    @Value("${spring.rabbitmq.queues.notification.exchange}")
-    private String notificationQueueExchange;
-
-    @Value("${spring.rabbitmq.queues.finance.name}")
-    private String financeQueueName;
-
-    @Value("${spring.rabbitmq.queues.finance.durable}")
-    private boolean financeQueueDurable;
-
-    @Value("${spring.rabbitmq.queues.finance.routing-key}")
-    private String financeQueueRoutingKey;
-
-    @Value("${spring.rabbitmq.queues.finance.exchange}")
-    private String financeQueueExchange;
-
-    @Value("${spring.rabbitmq.queues.log.name}")
-    private String logQueueName;
-
-    @Value("${spring.rabbitmq.queues.log.durable}")
-    private boolean logQueueDurable;
-
-    @Value("${spring.rabbitmq.queues.log.routing-key}")
-    private String logQueueRoutingKey;
-
-    @Value("${spring.rabbitmq.queues.log.exchange}")
-    private String logQueueExchange;
 
     public void send(String exchange, String routingKey, Payment payment) {
         log.info("Start queueing: Exchange = {}, RoutingKey = {}, Payment = {}", exchange, routingKey, payment.getPaymentId());
@@ -77,9 +34,4 @@ public class RabbitMQSender {
     public void send(Payment payment) {
         this.send(defaultQueueExchange, defaultQueueRoutingKey, payment);
     }
-
-    public void sendToFinance(Payment payment) {
-        this.send(financeQueueExchange, financeQueueRoutingKey, payment);
-    }
-
 }
